ef4e134ee4b229c47891612e5adf7d0fec1606d8
eatdreamcat <meow.std@outlook.com>
2023-12-05 00:36:23 +0800

    quad

--- RayTracingNext/quad.h
+++ RayTracingNext/quad.h
@@ -11,6 +11,10 @@ public:
     quad(const point3& _Q, const vec3& _u, const vec3& _v, shared_ptr<material> m)
         : Q(_Q), u(_u), v(_v), mat(m)
     {
+        auto n = cross(u, v);
+        normal = normalize(n);
+        D = dot(normal, Q);
+        w = n / dot(n, n);
         set_bounding_box();
     }
 
@@ -20,8 +24,46 @@ public:
 
     aabb bounding_box() const override { return bbox; }
 
-    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        return false; // To be implemented
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override
+    {
+        auto denom = dot(normal, r.direction());
+
+        // No hit if the ray is parallel to the plane.
+        if (fabs(denom) < 1e-8)
+            return false;
+
+        // Return false if the hit point parameter t is outside the ray interval.
+        auto t = (D - dot(normal, r.origin())) / denom;
+        if (!ray_t.contains(t))
+            return false;
+
+        // Determine the hit point lies within the planar shape using its plane coordinates.
+        auto intersection = r.at(t);
+        vec3 planar_hitpt_vector = intersection - Q;
+        auto alpha = dot(w, cross(planar_hitpt_vector, v));
+        auto beta = dot(w, cross(u, planar_hitpt_vector));
+
+        if (!is_interior(alpha, beta, rec))
+            return false;
+
+        rec.t = t;
+        rec.p = intersection;
+        rec.mat = mat;
+        rec.set_face_normal(r, normal);
+
+        return true;
+    }
+
+    virtual bool is_interior(double a, double b, hit_record& rec) const {
+        // Given the hit point in plane coordinates, return false if it is outside the
+        // primitive, otherwise set the hit record UV coordinates and return true.
+
+        if ((a < 0) || (1 < a) || (b < 0) || (1 < b))
+            return false;
+
+        rec.u = a;
+        rec.v = b;
+        return true;
     }
 
 private:
@@ -29,4 +71,8 @@ private:
     vec3 u, v;
     shared_ptr<material> mat;
     aabb bbox;
+
+    vec3 normal;
+    double D;
+    vec3 w;
 };
